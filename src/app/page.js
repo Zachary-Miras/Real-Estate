90b2a06e6385f36ac63831dbf78f46dc828a3875
Exalia <97191797+Exalia-Exa@users.noreply.github.com>
2024-09-14 18:37:43 +0200

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import { Header } from "../Components/Header";
+import { Item } from "../Components/Item";
+export default function Home() {
+	return (
+		<div>
+			<Header />
+
+			<Item />
+		</div>
+	);
+}
