beaf3a4ab261d535d36b135f5e1d063728046324
Exalia <zmiras51@gmail.com>
2024-09-22 23:04:33 +0200

    property services

--- src/services/propertyService.js
+++ src/services/propertyService.js
@@ -1,7 +1,3 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
 export async function createProperty() {
 	const newAddress = await prisma.address.create({
 		data: {
